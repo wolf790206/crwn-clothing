e8f2530724f022ed88f69a77e1fe4fa44ea3dfb6
wolf790206 <85617560+wolf790206@users.noreply.github.com>
2025-08-01 18:59:29 +0800

    update new lesson
    -   update App.jsx : add react-router Outlet
    -   update App.jsx : add navigation & shop
    -   update App.jsx : update route code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Outlet } from 'react-router';
 
 import Home from '@routers/home/home.component';
+
+const Navigation = () => {
+	return (
+		<div>
+			<div>I am the Navigation Bar</div>
+			<Outlet />
+		</div>
+	);
+};
+
+const Shop = () => {
+	return <div>I am the Shop Page</div>;
+};
 
 function App() {
 	return (
 		<Routes>
 			<Route
 				path="/"
-				element={<Home />}
-			/>
+				element={<Navigation />}
+			>
+				<Route
+					index
+					element={<Home />}
+				/>
+				<Route
+					path="shop"
+					element={<Shop />}
+				/>
+			</Route>
 		</Routes>
 	);
 }
