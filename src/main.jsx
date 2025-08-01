b3095041bf2b67dbdf285801aebde3fca033bccd
wolf790206 <85617560+wolf790206@users.noreply.github.com>
2025-08-01 19:01:45 +0800

    update lesson

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, HashRouter } from 'react-router';
 import './index.scss';
 import App from './App.jsx';
 
